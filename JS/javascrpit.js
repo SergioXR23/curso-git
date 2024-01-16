2af5aef42ba1c4eee125869bf632b7b20cbbe78c
SergioXR23 <4186935@alu.murciaeduca.es>
2024-01-16 09:18:24 +0100

    Este es el tercer commit corregido

--- JS/javascrpit.js
+++ JS/javascrpit.js
@@ -2,4 +2,6 @@ function saludo(){
     alert ("Hola Mundo")
     alert("Este es mi COMMIT 1")
     console.log("Esto es un log del COMMIT 1")
+    console.log("Esto es un log del COMMIT 3")
+    alert("Este es mi COMMIT 3 del segundo trabajo de control de versiones")
 }
